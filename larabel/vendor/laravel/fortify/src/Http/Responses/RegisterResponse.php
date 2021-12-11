570392f53e00ad036d07cb7fc3580685f89e1a77
bunga29 <bungafairuzzz@gmail.com>
2021-12-11 17:30:05 +0700

    push vendor sama env

---
+++ larabel/vendor/laravel/fortify/src/Http/Responses/RegisterResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Laravel\Fortify\Http\Responses;
+
+use Illuminate\Http\JsonResponse;
+use Laravel\Fortify\Contracts\RegisterResponse as RegisterResponseContract;
+use Laravel\Fortify\Fortify;
+
+class RegisterResponse implements RegisterResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        // dd($request->wantsJson());
+        return $request->wantsJson()
+                    ? new JsonResponse('', 201)
+                    : redirect()->route('home');
+    }
+}
